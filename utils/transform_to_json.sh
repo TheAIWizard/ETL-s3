af7dcc814fc8d2bdc667e596c47655a4b73b536e
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-05 17:17:56 +0000

    found solution to handle files

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -1,13 +1,12 @@
 SOURCE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/"
 ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/Archive annotations/"
 
-# Transform and save batch data to annotate
-python transform_to_json.py $SOURCE_PATH
-
 # Retrieve activity description to annotate and archive them
 mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
     echo "Moving file: $filename"
-    # Uncomment the following line when you're ready to perform the actual move
+    # Transform and save batch data to annotate
+    python transform_to_json.py "$SOURCE_PATH$filename"
+    # Move the treated batch data to archive
     mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
 done
 
