187933813dcbffcacd5d1104fe3df592443c0703
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-08 13:37:07 +0000

    pwd ls

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -1,15 +1,12 @@
 SOURCE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/"
 ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/Archive annotations/"
 
-# Connect to MinIO bucket
-export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-
-
 # Retrieve activity description to annotate and archive them
 mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
     echo "Moving file: $filename"
     # Retrieves fiactivity description from s3
     mc cp --recursive "$SOURCE_PATH$filename" ./
+    pwd
     # Transform and save batch data to annotate
     python transform_to_json.py $filename
     # Move the treated batch data to archive
