8e1bb5302c123cc08d342fb8ee034d013e5085af
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-08 13:31:54 +0000

    move mc host to sh

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -1,6 +1,10 @@
 SOURCE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/"
 ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/Archive annotations/"
 
+# Connect to MinIO bucket
+export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
+
+
 # Retrieve activity description to annotate and archive them
 mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
     echo "Moving file: $filename"
