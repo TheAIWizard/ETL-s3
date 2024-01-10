719682089556adc10bdf50e80c8e80a4cfa2874d
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-10 13:52:46 +0000

    deal with syntax

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -4,11 +4,10 @@ ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuell
 # Retrieve activity description to annotate and archive them
 mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
     echo "Moving file: $filename"
-    # Retrieves fiactivity description from s3
+    # Retrieves activity description from s3
     mc cp --recursive "$SOURCE_PATH$filename" ./
     # Transform and save batch data to annotate
-    python transform_to_json.py $filename
+    python transform_to_json.py "$filename"
     # Move the treated batch data to archive
     mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
 done
-
