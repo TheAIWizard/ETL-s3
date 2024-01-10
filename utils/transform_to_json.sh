ce57cb264872c3ac0e43c9a0008ce6389bb2b940
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-10 17:04:09 +0000

    change sh

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -4,14 +4,23 @@ SOURCE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle
 ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/Archive annotations/"
 
 # Retrieve activity description to annotate and archive them
-mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
-    export filename=$filename
-    [[ "$filename" == *.csv || "$filename" == *.parquet ]] && echo "Moving file: $filename" || echo "$filename is not a file to annotate"
-    echo "$SOURCE_PATH$filename"
-    # Retrieves activity description from s3
-    [[ "$filename" == *.csv || "$filename" == *.parquet ]] && mc cp --recursive "$SOURCE_PATH$filename" ./
-    # Transform and save batch data to annotate
-    python transform_to_json.py $filename
-    # Move the treated batch data to archive
-    [[ "$filename" == *.csv || "$filename" == *.parquet ]] && mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
+mc ls "$SOURCE_PATH" | while read -r filename; do
+    case "$filename" in
+        *.csv | *.parquet)
+            echo "Moving file: $filename"
+            echo "$SOURCE_PATH$filename"
+            
+            # Retrieves activity description from s3
+            mc cp --recursive "$SOURCE_PATH$filename" ./
+            
+            # Transform and save batch data to annotate
+            python transform_to_json.py "$filename"
+            
+            # Move the treated batch data to archive
+            mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
+            ;;
+        *)
+            echo "$filename is not a file to annotate"
+            ;;
+    esac
 done
