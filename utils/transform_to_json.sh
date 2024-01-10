9011265b69bc398abbcf4de9b0770529105676f9
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-10 14:03:50 +0000

    no problem it was because there was no file

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -2,13 +2,12 @@ SOURCE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle
 ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/Archive annotations/"
 
 # Retrieve activity description to annotate and archive them
-mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; 
-do
+mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
     echo "Moving file: $filename"
     # Retrieves activity description from s3
     mc cp --recursive "$SOURCE_PATH$filename" ./
     # Transform and save batch data to annotate
-    python transform_to_json.py $filename
+    python transform_to_json.py "$filename"
     # Move the treated batch data to archive
     mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
 done
