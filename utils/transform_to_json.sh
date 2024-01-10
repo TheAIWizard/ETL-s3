e801151e2c406d8224b70358359bdd2427f86fe3
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-10 14:41:47 +0000

    csv parquet check

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -3,12 +3,12 @@ ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuell
 
 # Retrieve activity description to annotate and archive them
 mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
-    [ ! -z "$filename" ] && echo "Moving file: $filename" || echo "No files to annotate"
+    [[ "$filename" == *.csv || "$filename" == *.parquet ]] && echo "Moving file: $filename" || echo "No files to annotate"
     echo "$SOURCE_PATH$filename"
     # Retrieves activity description from s3
-    #[ -n "$filename" ] && mc cp --recursive "$SOURCE_PATH$filename" ./
+    [[ "$filename" == *.csv || "$filename" == *.parquet ]] && mc cp --recursive "$SOURCE_PATH$filename" ./
     # Transform and save batch data to annotate
     python transform_to_json.py $filename
     # Move the treated batch data to archive
-    #[ -n "$filename" ] && mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
+    [[ "$filename" == *.csv || "$filename" == *.parquet ]] && mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
 done
