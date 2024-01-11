20d7017cfa5fe8f63ea216e3f0d1aa097da3baa9
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-11 15:18:15 +0000

    test handling empty files

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -5,23 +5,28 @@ SOURCE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle
 ARCHIVE_PATH="s3/nrandriamanana/Label Studio/Annotation APE 2024/Extract manuelle/Archive annotations/"
 
 # Retrieve activity description to annotate and archive them
-mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}' | while read -r filename; do
-    case "$filename" in
-        *.csv | *.parquet)
-            echo "Moving file: $filename"
-            echo "$SOURCE_PATH$filename"
-            
-            # Retrieves activity description from s3
-            mc cp --recursive "$SOURCE_PATH$filename" ./
-            
-            # Transform and save batch data to annotate
-            python transform_to_json.py "$filename"
-            
-            # Move the treated batch data to archive
-            mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
-            ;;
-        *)
-            echo "$filename is not a file to annotate"
-            ;;
-    esac
-done
+files=$(mc ls "$SOURCE_PATH" | grep -E '\.csv$|\.parquet$' | awk '{print $6}')
+if [ -n "$files" ]; then
+    echo "Files found. Starting processing..."
+    echo "$files" | while read -r filename; do
+        case "$filename" in
+            *.csv|*.parquet)
+                echo "Moving file: $filename"
+                
+                # Retrieve activity description from s3
+                mc cp --recursive "$SOURCE_PATH$filename" ./
+                
+                # Transform and save batch data to annotate
+                python transform_to_json.py "$filename"
+                
+                # Move the treated batch data to the archive
+                mc mv "$SOURCE_PATH$filename" "$ARCHIVE_PATH"
+                ;;
+            *)
+                echo "$filename is not a file to annotate"
+                ;;
+        esac
+    done
+else
+    echo "No files found for processing."
+fi
