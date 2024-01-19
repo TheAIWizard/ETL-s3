6c16b7b9f90910f866e454b2991e3d8c5200757f
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-19 13:37:08 +0000

    print id

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -39,5 +39,5 @@ fi
 # Create target S3 and export it just in case
 #export ID_S3_TARGET=$(python s3_create_target.py "$TARGET_PATH")
 # Sync target S3
-echo "$ID_S3_TARGET" #"84" 
-python s3_sync_target.py "2" "$TARGET_PATH"
+echo $ID_S3_TARGET #"84" 
+python s3_sync_target.py $ID_S3_TARGET "$TARGET_PATH"
