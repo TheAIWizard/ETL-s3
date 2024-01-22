78a5662fd9ca85a12c1fc62e961e3c49c7005f80
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-22 14:32:25 +0000

    archive done batch annotation

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -17,6 +17,11 @@ if [ -n "$files" ]; then
                 
                 # Retrieve activity description from s3
                 mc cp --recursive "$SOURCE_PATH$filename" ./
+
+                # Get export folder name for annotated data before creation
+                NUMERO_LOT=$(python get_last_target_folder_id.py)
+                # Copy in an other folder, tasks done in the current batch before creation of new LS project (avoid sync problem)
+                mc cp --recursive "$S3_BUCKET_PREFIX_ANNOTATION_TARGET/Lot $NUMERO_LOT" "$S3_BUCKET_PREFIX_ANNOTATION_TARGET/Lot $NUMERO_LOT termine"
                 
                 # Create or update/create label studio project
                 python update_create_project.py
