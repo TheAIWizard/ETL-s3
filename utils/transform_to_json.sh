b7e140eb017870d5cb3e2e49c869c772b080c070
TheAIWizard <nathanpanoel@hotmail.fr>
2024-01-22 18:25:54 +0000

    correct dev null

--- utils/transform_to_json.sh
+++ utils/transform_to_json.sh
@@ -30,7 +30,7 @@ if [ -n "$files" ]; then
                 NUMERO_LOT=$(python display_current_target_folder_id.py)
                 echo "Folder to sync: Lot $NUMERO_LOT"
                 # Create export folder to avoid unnecessary bugs if it works without this
-                mc cp /dev/null "s3/$S3_BUCKET/$S3_BUCKET_PREFIX_ANNOTATION_TARGET/Lot $NUMERO_LOT/"
+                mc cp /dev/null "s3$S3_BUCKET/$S3_BUCKET_PREFIX_ANNOTATION_TARGET/Lot $NUMERO_LOT/"
 
                 # Get current project id 
                 LABEL_STUDIO_PROJECT_ID=$(python count_project_id.py)
