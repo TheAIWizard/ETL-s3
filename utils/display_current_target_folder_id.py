553e5470d61dae97301492b44c9cca4f3bab8b6d
TheAIWizard <nathanpanoel@hotmail.fr>
2024-01-23 13:36:35 +0000

    manage display last pk

--- utils/display_current_target_folder_id.py
+++ utils/display_current_target_folder_id.py
@@ -1,11 +1,11 @@
 from get_last_target_folder_id import get_highest_integer_from_folders, get_folders_number_in_prefix
-from count_project_id import count_projects
+from count_project_id import get_last_pk
 
 if get_folders_number_in_prefix()-count_projects() > 1:
     print("Missing projects")
 if get_folders_number_in_prefix()-count_projects() == 1:
-    print(get_highest_integer_from_folders())
-if get_folders_number_in_prefix()-count_projects() == 0:
     print(get_highest_integer_from_folders()+1)
+if get_folders_number_in_prefix()-count_projects() == 0:
+    print(get_highest_integer_from_folders())
 if get_folders_number_in_prefix()-count_projects() < 0:
-    print("Missing lot files")
+    print("Missing export storage files")
