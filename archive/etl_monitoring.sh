821446ae382184978b719ad5eb9c5a6b0bfc48c6
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-02-21 18:40:59 +0000

    test

--- archive/etl_monitoring.sh
+++ archive/etl_monitoring.sh
@@ -1,1 +1,1 @@
-mc cp --recursive s3/projet-ape/label-studio/annotation-campaign-2024/NAF2008/data-annotated/completed/batch-2/ ./batch-2
+mc cp --recursive s3/projet-ape/label-studio/annotation-campaign-2024/NAF2008/data-annotated/completed/batch-1/ ./batch-1
